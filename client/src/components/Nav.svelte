1e2614d392d86b61963be6a391bf63e2bd7419dc
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2022-12-11 22:26:28 -0500

    feat: highlight active bar in nav

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -1,5 +1,32 @@
-<nav class="flex flex-col gap-3 bg-slate-700 items-center w-36 h-screen">
-	<a href="/">Videos</a>
-	<a href="/upload">Upload</a>
-	<a href="/about">About</a>
+<script lang="ts">
+	import { page } from "$app/stores";
+
+	$: currentPath = $page.url.pathname;
+
+	const paths = [
+		{href: '/', name: 'Videos'},
+		{href: '/upload', name: 'Upload'},
+		{href: '/about', name: 'About'},
+	]
+</script>
+
+<nav>
+	{#each paths as path}
+		<a class={path.href === currentPath ? 'active' : ''} href={path.href}>{path.name}</a>
+	{/each}
 </nav>
+
+<style lang="postcss">
+	nav {
+		@apply flex flex-col gap-3 bg-slate-700 items-center w-36 h-screen;
+	}
+	a {
+		@apply w-full text-center py-3;
+	}
+	a:hover {
+		@apply bg-slate-500 rounded-md;
+	}
+	.active {
+		@apply bg-slate-600;
+	}
+</style>
